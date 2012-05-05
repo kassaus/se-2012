454e06f7b8aacadbec6a19183485cd14cf7a36cb
pppluis@gmail.com <pppluis@gmail.com@0604fa44-e5ab-b8ff-0464-34fb6076a435>
2012-05-05 16:20:02 +0000

    Inicio do Inferno!!!

---
+++ Colecoes/src/se/colecoes/Colecoes.java
@@ -0,0 +1,13 @@
+package se.colecoes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Colecoes extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
