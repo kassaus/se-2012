877942d41e794c61008998204ded38a92e0b862e
Nelson.Manso@gmail.com <Nelson.Manso@gmail.com@0604fa44-e5ab-b8ff-0464-34fb6076a435>
2012-07-12 20:07:29 +0000

    Apagar Criado por mim por erro

--- trunk/GesColecoes/gen/se/trab/gescolecoes/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package se.trab.gescolecoes;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- trunk/GesColecoes/src/se/trab/gescolecoes/HTTP/HttpData.java
+++
@@ -1,8 +0,0 @@
-package se.trab.gescolecoes.HTTP;
-
-import java.util.Hashtable;
-public class HttpData {
-      public String content;
-      public Hashtable cookies = new Hashtable();
-      public Hashtable headers = new Hashtable();
-}
